bb14d92df2a9586a94e073e34ebe53f9f3501744
muddunazar <mudassarnazar3010@gmail.com>
2025-11-06 23:32:16 +0530

    correct logs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,9 @@ import * as React from "react"
 
 export default function Home() {
   const {GATSBY_A} = process.env
-  console.log('>>>logg',GATSBY_A)
-  console.log('>>>>>',process.env.GATSBY_A)
+  const rightway = process.env.GATSBY_A
+  console.log('>>>key in var',GATSBY_A)
+  console.log('>>>key like our project',GATSBY_A)
+  console.log('>>>>>simple log',process.env.GATSBY_A)
   return <div>Hello world!</div>
 }
