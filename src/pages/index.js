9dbb2436fc447f21238d74ffe879f20178414948
muddunazar <mudassarnazar3010@gmail.com>
2025-11-06 23:25:46 +0530

    tttt

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import * as React from "react"
 
 export default function Home() {
+  const {GATSBY_A} = process.env
+  console.log('>>>logg',GATSBY_A)
   console.log('>>>>>',process.env.GATSBY_A)
   return <div>Hello world!</div>
 }
